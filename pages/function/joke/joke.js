ab2006e5e78e947aca23b4a5573bd548914f9492
a1953176613 <1953176613@qq.com>
2018-12-07 12:24:06 +0800

    add joke

--- pages/function/joke/joke.js
+++ pages/function/joke/joke.js
@@ -5,7 +5,22 @@ Page({
    * Page initial data
    */
   data: {
+    joke_content: ''
+  },
 
+  bindRefresh: function() {
+    let that = this;
+    wx.request({
+      url: 'http://v.juhe.cn/joke/randJoke.php',
+      data: {
+        key: '0d8c0c2416d8adda070917bfe3fa4643'
+      },
+      success: function(res) {
+        that.setData({
+          joke_content: res.data.result[0].content
+        })
+      }
+    })
   },
 
   /**
